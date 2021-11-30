afb3d14c67e41627a565d4a3dd12b92f57dc63ee
Clive Drowley <christopherdrowley@gmail.com>
2021-11-30 23:13:02 +0000

    test indexing and reverse indexing

--- tests/test_unordered_list.py
+++ tests/test_unordered_list.py
@@ -15,7 +15,48 @@ def setup_unordered_list():
     return ul, ul_with_init_items, ul_with_added_items
 
 
+def test_getIndexElement(setup_unordered_list):
+    ul, ul_with_init_items, ul_with_added_items = setup_unordered_list
 
+    for test in [-100, -1 , 0, 1, 100]:
+        with pytest.raises(Exception) as e_info:
+            ul.getIndexElement(test)
+        assert str(e_info.value) == 'Index out of range'
+   
+
+    # assuming insertion order (last item added is at index 0))    
+    assert ul_with_init_items.getIndexElement(0).getData() == 54
+    assert ul_with_init_items.getIndexElement(1).getData() == 26
+    assert ul_with_init_items.getIndexElement(2).getData() == 93
+    assert ul_with_init_items.getIndexElement(3).getData() == 17
+    assert ul_with_init_items.getIndexElement(4).getData() == 77
+    assert ul_with_init_items.getIndexElement(5).getData() == 31
+
+    assert ul_with_init_items.getIndexElement(-1).getData() == 31
+    assert ul_with_init_items.getIndexElement(-2).getData() == 77
+    assert ul_with_init_items.getIndexElement(-3).getData() == 17
+    assert ul_with_init_items.getIndexElement(-4).getData() == 93
+    assert ul_with_init_items.getIndexElement(-5).getData() == 26
+    assert ul_with_init_items.getIndexElement(-6).getData() == 54
+
+    assert ul_with_added_items.getIndexElement(0).getData() == ['x', 'y', 'z']
+    assert ul_with_added_items.getIndexElement(1).getData() is None
+    assert ul_with_added_items.getIndexElement(2).getData() == 4
+    assert ul_with_added_items.getIndexElement(3).getData() == 3
+    assert ul_with_added_items.getIndexElement(4).getData() == 'B'
+    assert ul_with_added_items.getIndexElement(5).getData() == 'A'
+
+    assert ul_with_added_items.getIndexElement(-1).getData() == 'A'
+    assert ul_with_added_items.getIndexElement(-2).getData() == 'B'
+    assert ul_with_added_items.getIndexElement(-3).getData() == 3
+    assert ul_with_added_items.getIndexElement(-4).getData() == 4
+    assert ul_with_added_items.getIndexElement(-5).getData() is None
+    assert ul_with_added_items.getIndexElement(-6).getData() == ['x', 'y', 'z']
+
+    for test in [-100, -7, 6, 100]:
+        with pytest.raises(Exception) as e_info:
+            ul_with_added_items.getIndexElement(test)
+        assert str(e_info.value) == 'Index out of range'
 
 # print(mylist.size())
 # print(mylist.search(93))
