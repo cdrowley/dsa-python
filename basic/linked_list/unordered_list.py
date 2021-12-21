18af505711675583d9851a6d580a8ba0f69db9ba
Clive Drowley <christopherdrowley@gmail.com>
2021-12-21 00:32:32 +0000

    improves iteration method

--- basic/linked_list/unordered_list.py
+++ basic/linked_list/unordered_list.py
@@ -2,63 +2,68 @@
 
 from __future__ import annotations
 from typing import Any
+
 try:
     from basic.linked_list.node import Node
 except ModuleNotFoundError:
     from node import Node
 
+
 class UnorderedList:
     def __init__(self, *items: Any) -> None:
         self.head = None
+        self.tail = None
         for item in items:
             self.add(item)
 
     def __str__(self) -> str:
-        elements = []
+        nodes = []
         current = self.head
 
         while current is not None:
-            elements.append(f"{current.data}-->")
+            nodes.append(str(current.getData()))
             current = current.getNext()
-        return "".join(elements + ["None"])
+        return "-->".join(nodes + ["None"])
 
     def __len__(self) -> int:
         return self.size()
 
     def __iter__(self) -> Iterator[Any]:
-        items = []
         current = self.head
-        count = 0
         while current is not None:
-            count += 1
-            items.append(current.getData())
+            yield current
             current = current.getNext()
-        return iter(items)
 
     def isEmpty(self) -> bool:
         return self.head is None
 
     def add(self, item: Any) -> None:
-        temp = Node(item)
-        temp.setNext(self.head)
-        self.head = temp
+        """Adds an items to the head of the list."""
+        if not isinstance(item, Node):
+            item = Node(item)
+
+        item.setNext(self.head)
+        self.head = item
+        if self.tail is None:
+            self.tail = item
 
     def size(self) -> int:
-        current = self.head
-        count = 0
-        while current is not None:
-            count += 1
-            current = current.getNext()
-        return count
+        # current = self.head
+        # count = 0
+        # while current is not None:
+        #     count += 1
+        #     current = current.getNext()
+        # return count
+        return len([1 for _ in self])
 
     def getIndexElement(self, idx: int) -> Node:
         ulsize = self.size()
-        
-        if idx < 0: # allows for negative indexing
-            idx =  idx + ulsize
+
+        if idx < 0:  # allows for negative indexing
+            idx = idx + ulsize
 
         if ulsize == 0 or idx >= ulsize or idx < 0:
-            raise IndexError('Index out of range')
+            raise IndexError("Index out of range")
 
         current = self.head
         count = 0
@@ -69,11 +74,15 @@ class UnorderedList:
             current = current.getNext()
 
     def search(self, item: Any) -> bool:
-        current = self.head
-        while current is not None:
-            if current.getData() == item:
+        # current = self.head
+        # while current is not None:
+        #     if current.getData() == item:
+        #         return True
+        #     current = current.getNext()
+        # return False
+        for node in self:
+            if node.getData() == item:
                 return True
-            current = current.getNext()
         return False
 
     def remove(self, item: Any) -> None:
@@ -85,16 +94,37 @@ class UnorderedList:
 
         while current is not None:
             if current.getData() == item:
-                if previous is None:
+                if previous is None:  # item is located at the head
                     self.head = current.getNext()
+                    if self.head is None:
+                        self.tail = None
                 else:
-                    previous.setNext(current.getNext()) 
+                    if current is self.tail:
+                        self.tail = previous
+                    previous.setNext(current.getNext())
                 return
             previous = current
             current = current.getNext()
-        
+
         if current is None:
             raise ValueError("Item not found.")
 
-if __name__ == '__main__':
-    pass
+
+if __name__ == "__main__":
+    ul = UnorderedList()
+
+    ul_with_init_items = UnorderedList(31)
+
+    ul_with_added_items = UnorderedList()
+    for item in ["B", 3, None, ["x", "y", "z"]]:
+        ul_with_added_items.add(item)
+
+    for node in ul_with_added_items:
+        pass
+
+    ul.add(1)
+
+
+# %%
+
+# %%
